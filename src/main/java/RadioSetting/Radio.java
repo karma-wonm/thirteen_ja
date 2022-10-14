352b6b214c7a80ba7766e093055dbf2b4f0e4f43
Nastya Sizykh <sns0299@gmail.com>
2022-10-15 01:46:56 +0300

    Add different Station

--- src/main/java/RadioSetting/Radio.java
+++ src/main/java/RadioSetting/Radio.java
@@ -1,19 +1,37 @@
 package RadioSetting;
 
 public class Radio {
+    private int maxStation = 9;
+    private int minStation = 0;
     private int currentStation;
+    private int maxVolume = 100;
+    private int minVolume = 0;
     private int currentVolume;
+
+    public Radio(int size){
+        this.maxStation = size - 1;
+    }
+    public Radio(){
+
+    }
 
     public int getCurrentStation() {
 
         return currentStation;
     }
 
+    public int getMaxStation(){
+        return maxStation;
+    }
+
+    public int getMinStation(){
+        return minStation;
+    }
     public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation > 9) {
+        if (newCurrentStation > maxStation) {
             return;
         }
-        if (newCurrentStation < 0) {
+        if (newCurrentStation < minStation) {
             return;
         }
         currentStation = newCurrentStation;
@@ -21,18 +39,18 @@ public class Radio {
 
 
     public void next(int currentStation) {
-        if (currentStation < 9) {
+        if (currentStation < maxStation) {
             this.currentStation = currentStation + 1;
         } else {
-            this.currentStation = 0;
+            this.currentStation = minStation;
         }
     }
 
     public void prev(int currentStation) {
-        if (currentStation > 0) {
+        if (currentStation > minStation) {
             this.currentStation = currentStation - 1;
         } else {
-            this.currentStation = 9;
+            this.currentStation = maxStation;
         }
     }
 
@@ -42,18 +60,18 @@ public class Radio {
     }
 
     public void increaseVolume(int currentVolume) {
-        if (currentVolume < 10) {
+        if (currentVolume < maxVolume) {
             this.currentVolume = currentVolume + 1;
         } else {
-            this.currentVolume = currentVolume * 1;
+            this.currentVolume = currentVolume;
         }
     }
 
     public void decreaseVolume(int currentVolume) {
-        if (currentVolume > 0) {
+        if (currentVolume > minVolume) {
             this.currentVolume = currentVolume - 1;
         } else {
-            this.currentVolume = currentVolume * 1;
+            this.currentVolume = currentVolume;
         }
     }
 }
--- src/test/java/RadioSetting/RadioTest.java
+++ src/test/java/RadioSetting/RadioTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class RadioTest {
+
+
     //11 тестов
 
     @ParameterizedTest
@@ -24,7 +27,7 @@ public class RadioTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/dataNext")
     public void nextStation(int currentStation, int expected) {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10);
         radio.next(currentStation);
 
         int actual = radio.getCurrentStation();
@@ -35,7 +38,7 @@ public class RadioTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/dataPrev")
     public void prevStation(int currentStation, int expected) {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10);
         radio.prev(currentStation);
 
         int actual = radio.getCurrentStation();
@@ -46,7 +49,7 @@ public class RadioTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/volumeIncrease")
     public void increaseVolume(int currentVolume, int expected) {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10);
         radio.increaseVolume(currentVolume);
 
         int actual = radio.getCurrentVolume();
@@ -57,11 +60,20 @@ public class RadioTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/volumeDescrease")
     public void descreaseVolume(int currentVolume, int expected) {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10);
         radio.decreaseVolume(currentVolume);
 
         int actual = radio.getCurrentVolume();
 
         assertEquals(expected, actual);
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/DifferentStation"
+    )
+    public void createADifferentStation(int size,int expected){
+        Radio radio = new Radio( size);
+
+        assertEquals(expected, radio.getMaxStation());
+        assertEquals(expected = 0,radio.getMinStation() );
     }
 }
