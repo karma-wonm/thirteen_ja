e2baaf5e650a7136dbcce15c3b6c25b798b9d4e6
Nastya Sizykh <sns0299@gmail.com>
2022-10-24 18:43:50 +0300

    LOMBOK

--- src/main/java/RadioSetting/Radio.java
+++ src/main/java/RadioSetting/Radio.java
@@ -1,77 +1,26 @@
 package RadioSetting;
 
+
+import lombok.*;
+
+
 public class Radio {
     private int maxStation = 9;
     private int minStation = 0;
     private int currentStation;
-    private int maxVolume = 100;
     private int minVolume = 0;
+    private int maxVolume = 100;
+
     private int currentVolume;
-
-    public Radio(int size){
-        this.maxStation = size - 1;
-    }
-    public Radio(){
-
-    }
-
-    public int getCurrentStation() {
-
-        return currentStation;
-    }
-
-    public int getMaxStation(){
-        return maxStation;
-    }
-
-    public int getMinStation(){
-        return minStation;
-    }
-    public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation > maxStation) {
-            return;
-        }
-        if (newCurrentStation < minStation) {
-            return;
-        }
-        currentStation = newCurrentStation;
-    }
-
-
-    public void next(int currentStation) {
-        if (currentStation < maxStation) {
-            this.currentStation = currentStation + 1;
-        } else {
-            this.currentStation = minStation;
-        }
-    }
-
-    public void prev(int currentStation) {
-        if (currentStation > minStation) {
-            this.currentStation = currentStation - 1;
-        } else {
-            this.currentStation = maxStation;
-        }
-    }
-
-    public int getCurrentVolume() {
-
-        return currentVolume;
-    }
-
-    public void increaseVolume(int currentVolume) {
-        if (currentVolume < maxVolume) {
-            this.currentVolume = currentVolume + 1;
-        } else {
-            this.currentVolume = currentVolume;
-        }
-    }
-
-    public void decreaseVolume(int currentVolume) {
-        if (currentVolume > minVolume) {
-            this.currentVolume = currentVolume - 1;
-        } else {
-            this.currentVolume = currentVolume;
-        }
+    private int size = 10;
+    public Radio(int maxStation, int minStation, int currentStation, int minVolume, int maxVolume, int currentVolume, int size) {
+        this.maxStation = maxStation;
+        this.minStation = minStation;
+        this.currentStation = currentStation;
+        this.minVolume = minVolume;
+        this.maxVolume = maxVolume;
+        this.currentVolume = currentVolume;
+        this.size = size;
     }
 }
+
