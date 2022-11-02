39ee3f67ab1bf97d5b2862ba737b5d91059f0c46
Nastya Sizykh <sns0299@gmail.com>
2022-11-02 21:30:53 +0300

    Add Annotation

--- src/main/java/RadioSetting/Radio.java
+++ src/main/java/RadioSetting/Radio.java
@@ -2,7 +2,9 @@ package RadioSetting;
 
 
 import lombok.*;
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 
 public class Radio {
     private int maxStation = 9;
@@ -14,113 +16,5 @@ public class Radio {
     private int currentVolume;
     private int size = 10;
 
-    public Radio() {
-    }
-
-    public Radio(int maxStation, int minStation, int currentStation, int minVolume, int maxVolume, int currentVolume, int size) {
-        this.maxStation = maxStation;
-        this.minStation = minStation;
-        this.currentStation = currentStation;
-        this.minVolume = minVolume;
-        this.maxVolume = maxVolume;
-        this.currentVolume = currentVolume;
-        this.size = size;
-    }
-
-    public Radio(int size) {
-        this.maxStation = size - 1;
-    }
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStationLombok(int currentStation) {
-        this.currentStation = currentStation;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        this.currentVolume = currentVolume;
-    }
-
-    public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation > maxStation) {
-            return;
-        }
-        if (newCurrentStation < minStation) {
-            return;
-        }
-        currentStation = newCurrentStation;
-    }
-
-
-    public void next(int currentStation) {
-        if (currentStation < maxStation) {
-            this.currentStation = currentStation + 1;
-        } else {
-            this.currentStation = minStation;
-        }
-    }
-
-    public void prev(int currentStation) {
-        if (currentStation > minStation) {
-            this.currentStation = currentStation - 1;
-        } else {
-            this.currentStation = maxStation;
-        }
-    }
-
-
-    public void increaseVolume(int currentVolume) {
-        if (currentVolume < maxVolume) {
-            this.currentVolume = currentVolume + 1;
-        } else {
-            this.currentVolume = currentVolume;
-        }
-    }
-
-    public void decreaseVolume(int currentVolume) {
-        if (currentVolume > minVolume) {
-            this.currentVolume = currentVolume - 1;
-        } else {
-            this.currentVolume = currentVolume;
-        }
-    }
 }
 
--- src/test/java/RadioSetting/RadioTest.java
+++ src/test/java/RadioSetting/RadioTest.java
@@ -11,69 +11,81 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RadioTest {
 
 
- //11 тестов
-
+// //11 тестов
     @ParameterizedTest
-    @CsvFileSource(resources = "/data.csv")
-    public void changeStationPlus(int newCurrentStation, int expected) {
-        Radio radio = new Radio();
-        radio.setCurrentStation(newCurrentStation);
+    @CsvFileSource(resources = "/lombok")
+public void currentStation(int currentStation, int expected) {
+    Radio radio = new Radio();
 
-        int actual = radio.getCurrentStation();
+    radio.setCurrentStation(currentStation);
 
-        assertEquals(expected, actual);
-    }
+    int actual = radio.getCurrentStation();
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/dataNext")
-    public void nextStation(int currentStation, int expected) {
-        Radio radio = new Radio(10);
-        radio.next(currentStation);
+    assertEquals(expected, actual);
+}
 
-        int actual = radio.getCurrentStation();
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/data.csv")
+//    public void changeStationPlus(int currentStation, int expected) {
+//        Radio radio = new Radio();
+//        radio.setCurrentStation(currentStation);
+//
+//        int actual = radio.getCurrentStation();
+//
+//        assertEquals(expected, actual);
+//    }
 
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/dataPrev")
-    public void prevStation(int currentStation, int expected) {
-        Radio radio = new Radio(10);
-        radio.prev(currentStation);
-
-        int actual = radio.getCurrentStation();
-
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/volumeIncrease")
-    public void increaseVolume(int currentVolume, int expected) {
-        Radio radio = new Radio(10);
-        radio.increaseVolume(currentVolume);
-
-        int actual = radio.getCurrentVolume();
-
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/volumeDescrease")
-    public void descreaseVolume(int currentVolume, int expected) {
-        Radio radio = new Radio(10);
-        radio.decreaseVolume(currentVolume);
-
-        int actual = radio.getCurrentVolume();
-
-        assertEquals(expected, actual);
-    }
-    @ParameterizedTest
-    @CsvFileSource(resources = "/DifferentStation"
-    )
-    public void createADifferentStation(int size,int expected){
-        Radio radio = new Radio( size);
-
-        assertEquals(expected, radio.getMaxStation());
-        assertEquals(expected = 0,radio.getMinStation() );
-    }
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/dataNext")
+//    public void nextStation(int currentStation, int expected) {
+//        Radio radio = new Radio(10);
+//        radio.next(currentStation);
+//
+//        int actual = radio.getCurrentStation();
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/dataPrev")
+//    public void prevStation(int currentStation, int expected) {
+//        Radio radio = new Radio(10);
+//        radio.prev(currentStation);
+//
+//        int actual = radio.getCurrentStation();
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/volumeIncrease")
+//    public void increaseVolume(int currentVolume, int expected) {
+//        Radio radio = new Radio(10);
+//        radio.increaseVolume(currentVolume);
+//
+//        int actual = radio.getCurrentVolume();
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/volumeDescrease")
+//    public void descreaseVolume(int currentVolume, int expected) {
+//        Radio radio = new Radio(10);
+//        radio.decreaseVolume(currentVolume);
+//
+//        int actual = radio.getCurrentVolume();
+//
+//        assertEquals(expected, actual);
+//    }
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/DifferentStation"
+//    )
+//    public void createADifferentStation(int size,int expected){
+//        Radio radio = new Radio( size);
+//
+//        assertEquals(expected, radio.getMaxStation());
+//        assertEquals(expected = 0,radio.getMinStation() );
+//    }
 }
